5c87e4f65d74de36ab104634ddab849b682baf78
Farid Vakili <fvakili@cpp.edu>
2022-10-19 20:14:30 -0700

    Implemented pointer functions

--- employeeTwo.c
+++ employeeTwo.c
@@ -32,7 +32,15 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
     return strcmp((char *) targetPtr, tableValuePtr->name);//const void *targetPtr ==> 
                                                            //typecast as char pointer then pass into strcmp() 
 }
-
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+{ 
+    return strcmp((char *) targetPtr, tableValuePtr->phone);
+}
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{ 
+    return * (double *) targetPtr != tableValuePtr->salary; 
+                                                          
+}  
 
 //These are called wrappers. These functions are what you will use in your main!!! 
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number) 
@@ -43,6 +51,13 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
 {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName); 
 }
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone) {
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary) 
+{ 
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeNumber); 
+} 
 
 //!!!!!!!!static functions are only usable in this file.
 //no other files can access static funcitons!!!!!!!!!!!!!!!
